b2ab190e0048618423fe5300ba0eb0ef996e2520
cmphayes <42660033+cmphayes@users.noreply.github.com>
2018-10-24 13:21:12 -0400

    Add project files.

---
+++ CMPH_Financial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CMPH_Financial.MvcApplication" Language="C#" %>
---
+++ CMPH_Financial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CMPH_Financial.Startup))]
+namespace CMPH_Financial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
